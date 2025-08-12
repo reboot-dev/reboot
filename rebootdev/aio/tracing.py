fed47cafe02691a720fca5c5e9a9e67510ddc7cc
Robert-Jan Huijsman <22160949+rjhuijsman@users.noreply.github.com>
2025-08-12 16:15:23 +0200

    Add `--tracing=jaeger` option to `rbt dev run` (#4798)
    * Update to latest (stable, 1.x) Jaeger
    
    * Add `--trace=jaeger` option to `rbt dev run`
    
    Setting `--trace=jaeger` will run Jaeger's Docker
    container in the background, and set the
    application environment variables to export traces
    to it.
    
    * Make traces developer-readable
    
    Before this commit, most entries in our traces
    were very Reboot-internal, useful to e.g. debug
    where a Sidecar performance issue was being
    caused, but not understandable to developers
    _using_ Reboot.
    
    This commit makes the traces developer-readable by
    default, by supressing most Reboot-internal spans
    and adding a few new ones.
    
    * Address review comments
    
    Also, rename `--trace` to `--tracing`, since I
    found myself continuously mistyping it that way.
    It also makes more sense, since `--trace` feels
    more like a way to indicate _what_ should be
    traced, not _how_ it should be traced.
    
    * Update goldens.

--- rebootdev/aio/tracing.py
+++ rebootdev/aio/tracing.py
@@ -2,7 +2,12 @@ import asyncio
 import opentelemetry.instrumentation.grpc
 import os
 import signal
-from contextlib import AbstractContextManager, _AsyncGeneratorContextManager
+from contextlib import (
+    AbstractContextManager,
+    _AsyncGeneratorContextManager,
+    contextmanager,
+)
+from enum import Enum
 from functools import wraps
 from grpc.aio import (
     ClientCallDetails,
@@ -24,11 +29,72 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from rebootdev.aio.once import Once
 from rebootdev.aio.signals import install_cleanup
+from rebootdev.settings import (
+    ENVVAR_RBT_NAME,
+    ENVVAR_REBOOT_NODEJS,
+    ENVVAR_REBOOT_TRACE_LEVEL,
+)
 from typing import Any, AsyncIterator, Callable, Optional
 
 logger = get_logger(__name__)
 
 provider: Optional[TracerProvider] = None
+
+
+class TraceLevel(Enum):
+    """
+    The importance of a given span.
+
+    OpenTelemetry does not have a concept of trace levels, but we can
+    create our own to help manage the verbosity of our traces. Most
+    notably we want to normally show only customer-relevant traces, but
+    when debugging Reboot itself we want to see more details.
+    """
+    CUSTOMER = 0
+    REBOOT_INTERNAL = 1
+
+
+_trace_level = TraceLevel.CUSTOMER
+
+# Whether the tracing should include python-specific traces (if True),
+# or whether it should omit these as Python is merely an implementation
+# detail (if False).
+_python_specific = False
+
+
+class FilteredBatchSpanProcessor(BatchSpanProcessor):
+    """
+    A BatchSpanProcessor that filters out Reboot-specific spans.
+
+    This is a workaround for the fact that OpenTelemetry's SDKs don't
+    always obey their own filters.
+    """
+
+    # TODO(rjh): this only works well if there are no child spans below
+    #            the spans being filtered out; otherwise the child spans
+    #            are orphaned. Instead of using this mechanism, we
+    #            should replace OpenTelemetry's gRPC tracing
+    #            interceptors with our own logic, so that we can filter
+    #            which gRPC spans get _created_ (like we already do for
+    #            other spans with the `level` parameter to e.g. the
+    #            `span()` method below).
+
+    def on_end(self, span):
+        # The `rpc.service` attribute is set by OpenTelemetry's gRPC
+        # instrumentation; it contains the fully qualified name of the
+        # gRPC service being called.
+        service_name = span.attributes.get("rpc.service")
+        if (
+            service_name is not None and
+            # Filter out calls to the Sidecar: they're not interesting
+            # to customers, cause a lot of noise on the dashboard, and
+            # since they're "leaf" spans they'll never have child spans
+            # (see the TODO above).
+            str(service_name).startswith("rbt.v1alpha1.Sidecar") and
+            _trace_level.value < TraceLevel.REBOOT_INTERNAL.value
+        ):
+            return
+        super().on_end(span)
 
 
 def force_flush_and_shutdown(*args, **kwargs):
@@ -37,12 +103,12 @@ def force_flush_and_shutdown(*args, **kwargs):
     if provider is None:
         return
 
-    # Before shutting down this process we must force-flush the provider to
-    # make sure all spans are recorded. If there are unflushed traces this
-    # may take a second or two, but without it short-lived processes (e.g.
+    # Before shutting down this process we must force-flush the provider
+    # to make sure all spans are recorded. If there are unflushed traces
+    # this may take a moment, but without it short-lived processes (e.g.
     # consensuses in unit tests) would never get to flush their traces.
     process_name = provider.resource.attributes.get("service.name")
-    logger.warning(
+    logger.debug(
         f"Force-flushing tracer for '{process_name}'; "
         "this may delay shutdown..."
     )
@@ -58,9 +124,34 @@ def _start(process_name: str):
     global provider
     assert provider is None, "Tracer provider already initialized"
 
+    try:
+        global _trace_level
+        _trace_level = TraceLevel[os.environ.get(
+            ENVVAR_REBOOT_TRACE_LEVEL,
+            "CUSTOMER",
+        ).upper()]
+        global _python_specific
+        # Show Python-specific traces only if the user code is in
+        # Python, or if the trace level is set to a high enough level
+        # that we want to see Python implementation details.
+        _python_specific = (
+            ENVVAR_REBOOT_NODEJS not in os.environ or
+            _trace_level == TraceLevel.REBOOT_INTERNAL
+        )
+    except KeyError as e:
+        # The `ENVVAR_REBOOT_TRACE_LEVEL` environment variable is an
+        # internal detail (not used by developers, only by maintainers),
+        # so having it set to the wrong value is an error that warrants
+        # a stack trace.
+        raise ValueError(
+            f"Invalid {ENVVAR_REBOOT_TRACE_LEVEL}: "
+            f"'{os.environ.get(ENVVAR_REBOOT_TRACE_LEVEL)}'; pick from: "
+            f"{', '.join([level.name for level in TraceLevel])}"
+        ) from e
+
     resource = Resource(attributes={SERVICE_NAME: process_name})
     provider = TracerProvider(resource=resource)
-    processor = BatchSpanProcessor(OTLPSpanExporter())
+    processor = FilteredBatchSpanProcessor(OTLPSpanExporter())
     provider.add_span_processor(processor)
 
     # Sets the global default tracer provider; any `ProxyTracer`s already
@@ -78,7 +169,26 @@ def _start(process_name: str):
 _start_once = Once(_start)
 
 
-def start(process_name: str):
+def start(
+    process_name: Optional[str] = None, consensus_id: Optional[str] = None
+):
+    if process_name is None:
+        # TODO(rjh): make sure all paths in the Cloud set this
+        #            environment variable (not just customer containers
+        #            using the `rbt serve` CLI, but also the bazel-built
+        #            images), and make sure that it is obeyed (i.e. the
+        #            CLI considers it an alternative to the `--name`
+        #            argument). Then replace the fallback below with an
+        #            `assert`.
+        process_name = os.environ.get(ENVVAR_RBT_NAME, "Reboot Application")
+
+    if consensus_id is not None:
+        # Consensus IDs have an application ID prefix; that's redundant
+        # with the (nicer) application name we already have, so strip
+        # it.
+        consensus_id = consensus_id.split("-", 1)[-1]
+        process_name = f"{process_name} (consensus {consensus_id})"
+
     _start_once(process_name)
 
 
@@ -98,7 +208,34 @@ def get_tracer(name: Optional[str] = None):
     return trace.get_tracer(name) if name is not None else _tracer
 
 
-def function_span(**span_kwargs) -> Callable:
+@contextmanager
+def span(
+    name: str,
+    level: TraceLevel = TraceLevel.REBOOT_INTERNAL,
+    python_specific: bool = False,
+    **span_kwargs,
+):
+    """
+    Start a new tracing span.
+    """
+    if _trace_level.value >= level.value and (
+        # Python-specific spans are only emitted if the customer is
+        # using Python code, OR if the trace level is high enough that
+        # we want to see Python implementation details.
+        (not python_specific or _python_specific) or
+        _trace_level == TraceLevel.REBOOT_INTERNAL
+    ):
+        with _tracer.start_as_current_span(name, **span_kwargs):
+            yield
+    else:
+        yield
+
+
+def function_span(
+    name: Optional[str] = None,  # Default: use the function's name.
+    level: TraceLevel = TraceLevel.REBOOT_INTERNAL,
+    **span_kwargs,
+) -> Callable:
     """
     Convenience decorator to create spans named after a function or method.
 
@@ -126,9 +263,12 @@ def function_span(**span_kwargs) -> Callable:
     def decorator(func: Callable) -> Callable:
 
         async def wrapper(*args, **kwargs):
-            global _tracer
-            with _tracer.start_as_current_span(
-                func.__qualname__ + "()",
+            with span(
+                name=(name or func.__qualname__) + "()",
+                level=level,
+                # These spans are Python-specific, since they use Python
+                # module/class/method names.
+                python_specific=True,
                 **span_kwargs,
             ):
                 return await func(*args, **kwargs)
@@ -138,7 +278,7 @@ def function_span(**span_kwargs) -> Callable:
     return decorator
 
 
-def main_span(name, **span_kwargs) -> Callable:
+def main_span(name: Optional[str] = None, **span_kwargs) -> Callable:
     """
     Convenience decorator to run synchronous 'main' functions in a span.
 
@@ -187,9 +327,12 @@ class TracedAsyncGeneratorContextManager(_AsyncGeneratorContextManager):
     2. An `@asynccontextmanager` backed by a generator.
     """
 
-    def __init__(self, func, args, kwargs, span_kwargs):
+    def __init__(
+        self, func, args, kwargs, span_level: TraceLevel, span_kwargs
+    ):
         # The OpenTelemetry context manager will be created in `__aenter__`.
         self.tracing: Optional[AbstractContextManager] = None
+        self._span_level = span_level
         # The `span_kwargs` are passed to the OpenTelemetry context manager when
         # it is created.
         self.span_kwargs = span_kwargs
@@ -201,21 +344,27 @@ class TracedAsyncGeneratorContextManager(_AsyncGeneratorContextManager):
         super().__init__(func=func, args=args, kwds=kwargs)
 
     async def __aenter__(self):
-        self.tracing = _tracer.start_as_current_span(
-            self.func_name,
-            **self.span_kwargs,
-        )
-        assert self.tracing is not None
-        self.tracing.__enter__()
+        global _trace_level
+        if _trace_level.value >= self._span_level.value:
+            self.tracing = _tracer.start_as_current_span(
+                self.func_name,
+                **self.span_kwargs,
+            )
+            assert self.tracing is not None
+            self.tracing.__enter__()
         return await super().__aenter__()
 
     async def __aexit__(self, exc_type, exc_value, traceback):
-        assert self.tracing is not None, "__aenter__ was not called"
-        self.tracing.__exit__(exc_type, exc_value, traceback)
+        if _trace_level.value >= self._span_level.value:
+            assert self.tracing is not None, "__aenter__ was not called"
+            self.tracing.__exit__(exc_type, exc_value, traceback)
         return await super().__aexit__(exc_type, exc_value, traceback)
 
 
-def asynccontextmanager_span(**span_kwargs) -> Callable:
+def asynccontextmanager_span(
+    level: TraceLevel = TraceLevel.REBOOT_INTERNAL,
+    **span_kwargs,
+) -> Callable:
     """
     Same as `function_span`, but for async context managers.
 
@@ -235,6 +384,7 @@ def asynccontextmanager_span(**span_kwargs) -> Callable:
                 func=func,
                 args=args,
                 kwargs=kwargs,
+                span_level=level,
                 span_kwargs=span_kwargs,
             )
 
@@ -270,14 +420,25 @@ def generator_span(**span_kwargs) -> Callable:
 def aio_server_interceptor(tracer_provider=None, filter_=None):
     return opentelemetry.instrumentation.grpc.aio_server_interceptor(
         tracer_provider=tracer_provider,
+        # TODO(rjh): We'd have liked to use the `service_prefix` filter
+        # to filter out Reboot-specific spans (i.e. any with the "rbt."
+        # prefix), but OpenTelemetry's SDKs don't (always) obey those
+        # filters!? We use a custom `FilteredBatchSpanProcessor`
+        # instead; see above.
         filter_=filter_,
     )
 
 
 def aio_client_interceptors():
-    return opentelemetry.instrumentation.grpc.aio_client_interceptors() + [
+    return opentelemetry.instrumentation.grpc.aio_client_interceptors(
+        # TODO(rjh): We'd have liked to use the `service_prefix` filter
+        # to filter out Reboot-specific spans (i.e. any with the "rbt."
+        # prefix), but OpenTelemetry's SDKs don't (always) obey those
+        # filters!? We use a custom `FilteredBatchSpanProcessor`
+        # instead; see above.
+    ) + [
         UnaryUnaryOpenTelemetryWorkaroundInterceptor(),
-        StreamUnaryOpenTelemetryWorkaroundInterceptor()
+        StreamUnaryOpenTelemetryWorkaroundInterceptor(),
     ]
 
 
@@ -324,3 +485,11 @@ class StreamUnaryOpenTelemetryWorkaroundInterceptor(
             return call
         except asyncio.CancelledError:
             raise
+
+
+def full_type_name(obj: Any) -> str:
+    """
+    Returns the fully qualified type name of an object, including its module.
+    """
+    assert hasattr(obj, "__class__"), "'obj' is not an object"
+    return f"{obj.__class__.__module__}.{obj.__class__.__qualname__}"
