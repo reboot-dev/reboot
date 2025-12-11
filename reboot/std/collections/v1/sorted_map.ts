082e8ba9432eab179a64bd930e150653d3928c48
Kat Fang <katfang@users.noreply.github.com>
2025-12-11 13:32:03 +0000

    Make SortedMap library accessible from Typescript

--- reboot/std/collections/v1/sorted_map.ts
+++ reboot/std/collections/v1/sorted_map.ts
@@ -1,4 +1,4 @@
-import { NativeServicer } from "@reboot-dev/reboot";
+import { NativeLibrary, NativeServicer } from "@reboot-dev/reboot";
 export * from "@reboot-dev/reboot-std-api/collections/v1/sorted_map_rbt.js";
 
 export default {
@@ -8,5 +8,11 @@ export default {
         nativeServicerModule: "reboot.std.collections.v1.sorted_map",
       },
     ];
+  },
+  sortedMapLibrary: (): NativeLibrary => {
+    return {
+      nativeLibraryModule: "reboot.std.collections.v1.sorted_map",
+      nativeLibraryFunction: "sorted_map_library",
+    };
   },
 };
