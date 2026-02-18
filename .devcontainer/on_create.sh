5e66942e6e77f8b228c72b8706f851c60fe3f452
Robert-Jan Huijsman <22160949+rjhuijsman@users.noreply.github.com>
2026-02-18 19:32:24 +0100

    Fix Codespace build (#5225)
    Before this commit, Codespaces would fail to build
    (boot in recovery mode) because when
    `on_create.sh` runs `git_config.sh` which runs
    `install_precommit_hook.sh`, the `devtools`
    submodule it relies on wouldn't be present yet.
    
    This commit fixes that by doing a `git submodule
    update --init --recursive` to initialize the
    submodules before running the git setup. Pulling
    the submodules on creation incidentally fixes that
    same UX issue for us developers too.

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -7,6 +7,9 @@ set -e # Exit if a command exits with an error.
 set -u # Treat expanding an unset variable as an error.
 set -x # Echo executed commands to help debug failures.
 
+# Make sure all of the submodules are here.
+git submodule update --init --recursive
+
 # Set up ease-of-use environmental preferences.
 .devcontainer/git_config.sh
 
