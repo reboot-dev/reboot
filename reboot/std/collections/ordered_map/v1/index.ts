fb5665a0f5273b18dddf41c61d4d759e3b2e6a36
Kat Fang <katfang@users.noreply.github.com>
2026-06-03 11:10:22 -0400

    OrderedMap available in React (#5435)
    * Library-ifying OrderedMap in Typescript.
    
    * OrderedMap docs: Setting up the library.
    
    * OrderedMap docs: Overriding authorizer.
    
    * Making React library for OrderedMap.
    
    * Test for using OrderedMap from React.
    
    * OrderedMap docs: React library.

--- reboot/std/collections/ordered_map/v1/index.ts
+++ reboot/std/collections/ordered_map/v1/index.ts
@@ -1,4 +1,5 @@
-import { NativeServicer } from "@reboot-dev/reboot";
+import { NativeLibrary, NativeServicer } from "@reboot-dev/reboot";
+import { OrderedMap } from "@reboot-dev/reboot-std-api/collections/ordered_map/v1/ordered_map_rbt.js";
 export * from "@reboot-dev/reboot-std-api/collections/ordered_map/v1/ordered_map_rbt.js";
 
 export default {
@@ -11,3 +12,21 @@ export default {
     ];
   },
 };
+
+export const ORDERED_MAP_LIBRARY_NAME =
+  "reboot.std.collections.ordered_map.v1.ordered_map";
+
+export function orderedMapLibrary({
+  authorizer,
+}: {
+  // Just using `OrderedMap.Authorizer` results in ts(2749), "refers to a value,
+  // but is being used as a type." `InstanceType<typeof ...>` allows us to
+  // refer to the type.
+  authorizer?: InstanceType<typeof OrderedMap.Authorizer>;
+} = {}): NativeLibrary {
+  return {
+    nativeLibraryModule: "reboot.std.collections.ordered_map.v1.ordered_map",
+    nativeLibraryFunction: "ordered_map_library",
+    authorizer,
+  };
+}
