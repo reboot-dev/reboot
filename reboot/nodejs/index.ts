118fa50fcb258df3334da4062fa679ab4586c41f
katfang <kat@katfang.com>
2026-07-13 15:18:48 -0400

    Libraries (general): Refactor to use multi-authorizer code path for all authorizers.

--- reboot/nodejs/index.ts
+++ reboot/nodejs/index.ts
@@ -890,7 +890,6 @@ export function isAppInternal({
 export type NativeLibrary = {
   nativeLibraryModule: string;
   nativeLibraryFunction: string;
-  authorizer?: Authorizer<unknown, unknown>;
   authorizers?: Record<string, Authorizer<unknown, unknown>>;
 };
 
