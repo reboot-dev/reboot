189ca29f29ff99bb03385ae34a8a18c3e737dd2e
katfang <kat@katfang.com>
2026-07-13 15:25:05 -0400

    presence.servicers -> presence_library(authorizer=allow())

--- tests/reboot/react/std/presence/test_against_local_envoy.py
+++ tests/reboot/react/std/presence/test_against_local_envoy.py
@@ -1,6 +1,7 @@
-import reboot.std.presence.v1.presence
 from reboot.aio.applications import Application
+from reboot.aio.auth.authorizers import allow
 from reboot.aio.memoize import MemoizeServicer
+from reboot.std.presence.v1.presence import presence_library
 from tests.reboot.react.std.presence.test import test
 from tests.reboot.react.test_against_local_envoy import (
     web_test_against_local_envoy,
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     web_test_against_local_envoy(
         test=test,
         application=Application(
-            servicers=[MemoizeServicer] +
-            reboot.std.presence.v1.presence.servicers(),
+            servicers=[MemoizeServicer],
+            libraries=[presence_library(authorizer=allow())],
         ),
     )
