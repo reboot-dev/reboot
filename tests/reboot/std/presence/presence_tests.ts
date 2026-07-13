61566068502ba3630c3cf0869293458cc759e0c8
katfang <kat@katfang.com>
2026-07-13 15:23:21 -0400

    Presence Library (TS): Allow passing 1 AuthorizerRule for all three authorizers

--- tests/reboot/std/presence/presence_tests.ts
+++ tests/reboot/std/presence/presence_tests.ts
@@ -4,6 +4,7 @@ import {
   ReaderContext,
   Reboot,
   TokenVerifier,
+  allow,
 } from "@reboot-dev/reboot";
 import { errors_pb } from "@reboot-dev/reboot-api";
 import { MousePosition } from "@reboot-dev/reboot-std/presence/mouse_tracker/v1";
@@ -17,7 +18,6 @@ class EmptyTokenVerifier extends TokenVerifier {
     context: ReaderContext,
     token?: string
   ): Promise<Auth | null> {
-    assert(context.appInternal);
     return new Auth({ userId: token || "default-user" });
   }
 }
@@ -101,4 +101,72 @@ test("Use Presence Servicers", async (t) => {
       top: 2,
     });
   });
+
+  await t.test("Fail Authorization If No Valid Token", async (t) => {
+    await rbt.up(
+      new Application({
+        libraries: [presenceLibrary()],
+      })
+    );
+
+    // Assert that Subscriber calls cannot be made.
+    try {
+      let context = rbt.createExternalContext("test-1");
+      await Subscriber.ref("test-id").create(context);
+      assert(false);
+    } catch (e) {
+      assert(e instanceof Subscriber.CreateAborted);
+      assert(e.code === 7);
+      assert(e.error instanceof errors_pb.PermissionDenied);
+    }
+
+    // Assert that Presence calls cannot be made.
+    try {
+      let context = rbt.createExternalContext("test-2");
+      await Presence.ref("test-id").subscribe(context, {
+        subscriberId: "test-id",
+      });
+      assert(false);
+    } catch (e) {
+      assert(e instanceof Presence.SubscribeAborted);
+      assert(e.code === 7);
+      assert(e.error instanceof errors_pb.PermissionDenied);
+    }
+
+    // Assert that MousePosition calls cannot be made.
+    try {
+      let context = rbt.createExternalContext("test-3");
+      await MousePosition.ref("mouse-position-test-id").update(context, {
+        left: 1,
+        top: 2,
+      });
+      assert(false);
+    } catch (e) {
+      assert(e instanceof MousePosition.UpdateAborted);
+      assert(e.code === 7);
+      assert(e.error instanceof errors_pb.PermissionDenied);
+    }
+  });
+
+  await t.test(
+    "Override default authorizer with allow AuthorizerRule",
+    async (t) => {
+      await rbt.up(
+        new Application({
+          libraries: [presenceLibrary({ authorizer: allow() })],
+        })
+      );
+
+      const context = rbt.createExternalContext("test");
+
+      // Should not error.
+      await Subscriber.ref("test-id").create(context);
+
+      // Should not error.
+      await MousePosition.ref("mouse-position-test-id").update(context, {
+        left: 1,
+        top: 2,
+      });
+    }
+  );
 });
