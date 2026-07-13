7954b4193593e053f15dfbbc9f7b2015d5ce828b
katfang <kat@katfang.com>
2026-07-13 15:25:05 -0400

    presence.servicers -> presence_library(authorizer=allow())

--- tests/reboot/presence_main.py
+++ tests/reboot/presence_main.py
@@ -3,15 +3,16 @@
 This is a main function for a container hosting only the `Presence` servicer.
 """
 import asyncio
-import reboot.std.presence.v1.presence
 from reboot.aio.applications import Application
+from reboot.aio.auth.authorizers import allow
 from reboot.aio.memoize import MemoizeServicer
+from reboot.std.presence.v1.presence import presence_library
 
 
 async def main():
     application = Application(
-        servicers=[MemoizeServicer] +
-        reboot.std.presence.v1.presence.servicers(),
+        servicers=[MemoizeServicer],
+        libraries=[presence_library(authorizer=allow())],
     )
     await application.run()
 
